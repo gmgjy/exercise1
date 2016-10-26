c32a725aea86c1a7fe9d57b638610254613f5c20
jamess90 <jamess90@naver.com>
2016-10-27 01:37:52 +0900

    주석 수정 및 추상클래스의 일반메소드 추가

--- src/exercise1/Car.java
+++ src/exercise1/Car.java
@@ -1,6 +1,7 @@
 package exercise1;
 
-public interface Car {			
-	public void is_ride();				//interface에서는 body 부분이 없다.
+public interface Car {					// 추상클래스가 추상 메소드를 반드시 오버라이딩 해야 하듯 모든 메소드를 자식 클래스에서 오버라이딩 해야 한다.
+	public void is_ride();				// interface에서는 body 부분이 없다.
 	public void refueling();
+	
 }
--- src/exercise1/ab_Car.java
+++ src/exercise1/ab_Car.java
@@ -1,9 +1,13 @@
 package exercise1;
 
-abstract class ab_Car {					
-	public abstract void run();			// 추상 클래스는 추상 메소드가 하나 이상 있어야 한다.
-										// body부분이 없더라도 {}가 존재하면 추상클래스가 아니다.
-	public void refuel(String name) {	// 추상클래스에서 추상 메소드가 아닌 일반 메소드도 사용할 수 있다.
+abstract class ab_Car { // 추상 메소드가 없더라도 abstract 키워드를 붙이면 추상 클래스이다.
+	public abstract void run(); // 추상 메소드가 있으면 자식 클래스에서 반드시 오버라이딩 해야 한다.
+								// body부분이 없더라도 {}가 존재하면 추상클래스가 아니다.
+
+	public void refuel(String name) { // 추상클래스에서 추상 메소드가 아닌 일반 메소드도 사용할 수 있다.
 		System.out.println(name + "이 주유중입니다.");
+	}
+
+	public void fix() { // 일반 메소드는 반드시 오버라이딩 할 필요는 없다.
 	}
 }
